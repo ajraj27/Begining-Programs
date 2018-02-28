d84578ee73a9e79ccbccc5d3f306e1fc074c771d
ajraj27 <rajanuj.iiita@gmail.com>
2018-02-28 14:10:35 +0530

    BST

---
+++ implementBST.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct bst{
+
+	int data;
+	struct bst* left;
+	struct bst* right;
+};
+
+typedef struct bst node;
+
+void insert(node** root_ref,int x)
+{
+
+	if(*root_ref==NULL){
+		node* temp=(node*)malloc(sizeof(node));
+		temp->data=x;
+		temp->left=NULL;
+		temp->right=NULL;
+		*root_ref=temp;
+		 return;
+		}
+	
+
+	else if(x<(*root_ref)->data)
+		insert(&((*root_ref)->left),x);
+
+	else
+		insert(&((*root_ref)->right),x);
+
+}
+
+int main()
+{	
+	int n,num,i;
+	scanf("%d",&n);
+	node* root=NULL;
+
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&num);
+		insert(&root,num);
+	}
+
+}
